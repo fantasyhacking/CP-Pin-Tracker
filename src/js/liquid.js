fd5cfbb4a2a64ba7f831b585a6567a514c0d8525
Lynx <titshacking@users.noreply.github.com>
2016-06-20 01:52:54 +0530

    Create liquid.js

---
+++ src/js/liquid.js
@@ -0,0 +1,12 @@
+$(function() {
+    $("#imgLiquidFill").imgLiquid({
+        fill: true,
+        horizontalAlign: "center",
+        verticalAlign: "top"
+    });    
+    $("#imgLiquidNoFill").imgLiquid({
+        fill: false,
+        horizontalAlign: "center",
+        verticalAlign: "50%"
+    });
+});
